7a989e5a6327f7bfac909964b6ed85edb5930c85
harveyk86 <harveyjk86+github@gmail.com>
2015-11-03 11:32:48 +0000

    Updated __main__ with Setup and Teardown funcationality

--- PythonREST/__main__.py
+++ PythonREST/__main__.py
@@ -1,7 +1,15 @@
-from PythonREST import Application
+from app import Setup
+from app import Application
+from app import Teardown
 
 def main():
-    application = Application()
-    application.run()
+    setup = Setup()
+    config = setup.run()
+
+    app = Application()
+    app.run(config)
+
+    teardown = Teardown()
+    teardown.run()
 
 main()
