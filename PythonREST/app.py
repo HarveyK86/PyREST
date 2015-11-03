df9b1c8e22443aafb5c022e10b4447cf8f17d3b1
harveyk86 <harveyjk86+github@gmail.com>
2015-11-03 12:58:55 +0000

    Updated app file, new parameters, more logging etc.

--- PythonREST/app.py
+++ PythonREST/app.py
@@ -1,16 +1,27 @@
 import re
 
+from welcome import welcome
+
 class Setup(object):
 
     __filename = "app.properties"
-    __regex = r"^([\w.-]*):(.*)$"
-    __keys = ("url.host", "url.route", "url.params")
+    __regex = r"^([\w-]*):(.*)$"
+    __keys = ("url-host", "url-route", "url-params")
 
     def __init__(self):
-        print("Setup : __init__")
+        #print("Setup : __init__")
+        pass
 
     def run(self):
         print("Setup : run")
+
+        config = self.__getConfig()
+
+        print("Setup : run[returns=" + str(config) + "]")
+        return config
+
+    def __getConfig(self):
+        print("Setup : __getConfig")
 
         file = open(self.__filename)
         lines = file.readlines()
@@ -22,7 +33,9 @@ class Setup(object):
             if match:
                 config[match.group(1)] = match.group(2)
             else:
-                print("Setup : run : no default config")
+                print("Setup : __getConfig : no default config")
+
+        print("----- Configuration -----")
 
         for key in self.__keys:
             if config.get(key):
@@ -36,20 +49,42 @@ class Setup(object):
             else:
                 raise RuntimeError("Value for " + key + " required!")
 
+        print("-------------------------")
+
+        print("Setup : __getConfig[returns=" + str(config) + "]")
         return config
 
 class Application(object):
 
-    def __init__(self):
-        print("Application : __init__")
+    __format = "{url-host}{url-route}?{url-params}"
 
-    def run(self, config):
-        print("Application : run[config=" + str(config) + "]")
+    def __init__(self, config):
+        print("Application : __init__[config=" + str(config) + "]")
+
+        self.__config = config
+
+    def run(self):
+        print("Application : run")
+
+        welcome()
+
+        url = self.__getURL()
+        print("Application : run : URL is " + url)
+
+    def __getURL(self):
+        #print("Application : __getURL")
+
+        url = self.__format.format(**self.__config)
+
+        print("Application : __getURL[returns=" + url + "]")
+        return url
 
 class Teardown(object):
 
     def __init__(self):
-        print("Teardown : __init__")
+        #print("Teardown : __init__")
+        pass
 
     def run(self):
-        print("Teardown : run")
+        #print("Teardown : run")
+        pass
