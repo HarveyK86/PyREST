c6cbf39ba90b5707f785eda4041bacbbfeaad3c0
harveyk86 <harveyjk86+github@gmail.com>
2015-11-03 12:57:49 +0000

    Added welcome file, for shiggles

---
+++ PythonREST/welcome.py
@@ -0,0 +1,10 @@
+def welcome():
+    print("\n\n`7MM\"\"\"Mq.            `7MM\"\"\"Mq.  `7MM\"\"\"YMM   .M\"\"\"bgd MMP\"\"MM\"\"YMM")
+    print("  MM   `MM.             MM   `MM.   MM    `7  ,MI    \"Y P'   MM   `7")
+    print("  MM   ,M9 `7M'   `MF'  MM   ,M9    MM   d    `MMb.          MM")
+    print("  MMmmdM9    VA   ,V    MMmmdM9     MMmmMM      `YMMNq.      MM")
+    print("  MM          VA ,V     MM  YM.     MM   Y  , .     `MM      MM")
+    print("  MM           VVV      MM   `Mb.   MM     ,M Mb     dM      MM")
+    print(".JMML.         ,V     .JMML. .JMM..JMMmmmmMMM P\"Ybmmd\"     .JMML.")
+    print("              ,V")
+    print("           OOb\"\n\n")
